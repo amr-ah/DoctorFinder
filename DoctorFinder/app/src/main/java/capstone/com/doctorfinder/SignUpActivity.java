46267963644a4ec519a5b44e455de72eca67ea54
anass-lahlal <anass.lahlal@gmail.com>
2018-03-13 23:28:04 +0300

    doctor's sign up page

--- DoctorFinder/app/src/main/java/capstone/com/doctorfinder/SignUpActivity.java
+++ DoctorFinder/app/src/main/java/capstone/com/doctorfinder/SignUpActivity.java
@@ -3,11 +3,18 @@ package capstone.com.doctorfinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import lib.kingja.switchbutton.SwitchMultiButton;
+
 public class SignUpActivity extends AppCompatActivity {
+
+    SwitchMultiButton mSwitchMultiButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mSwitchMultiButton = findViewById(R.id.radioDoc);
+        mSwitchMultiButton.setText("Patient","Doctor");
     }
 }
