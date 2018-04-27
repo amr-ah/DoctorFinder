42806c29f14c6dd0bc70ad5a5a1941cbfad42239
amr sharqawi <amro.ah@hotmail.com>
2018-04-27 15:41:20 +0300

    doctor sign up database configured

--- app/src/main/java/capstone/com/doctorfinder/DoctorSignup.java
+++ app/src/main/java/capstone/com/doctorfinder/DoctorSignup.java
@@ -1,27 +1,56 @@
 package capstone.com.doctorfinder;
 
 import android.content.Intent;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.MultiAutoCompleteTextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.roughike.swipeselector.SwipeItem;
 import com.roughike.swipeselector.SwipeSelector;
 
 public class DoctorSignup extends AppCompatActivity {
+
+    private MultiAutoCompleteTextView AddressTextView;
+    private AutoCompleteTextView WorkNumTextView;
+    private AutoCompleteTextView TagsTextView;
+    private Button AddTagButton;
+    private MultiAutoCompleteTextView BioTextView;
+    private Button FinishButton;
+
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_signup);
 
-        SwipeSelector swipeSelector = findViewById(R.id.swipeSelector);
+        firebaseAuth = FirebaseAuth.getInstance();
 
+        Intent doctor_signup = getIntent();
+        final String Email = doctor_signup.getStringExtra(SignUpActivity.EXTRA_EMAIL);
+        final String Password = doctor_signup.getStringExtra(SignUpActivity.EXTRA_PASS);
+        final String Name = doctor_signup.getStringExtra(SignUpActivity.EXTRA_NAME);
+        final String Num = doctor_signup.getStringExtra(SignUpActivity.EXTRA_NUM);
+
+                SwipeSelector swipeSelector = findViewById(R.id.swipeSelector);
         String[] categories = {"Swipe to select your category", "Allergist/Immunologist", "Anesthesiologist", "Cardiologist", "Dermatologist", "Family Physician",
-                "Gastroenterologist","Generalist", "Hematologist", "Internist", "Nephrologist",
+                "Gastroenterologist", "Generalist", "Hematologist", "Internist", "Nephrologist",
                 "Neurologist", "Ophthalmologist", "Pathologist", "Pediatrician", "Psychiatrist",
-                "Radiologist", "Rheumatologist", "Sports Medicine Specialist", "Urologist","Other"};
-
+                "Radiologist", "Rheumatologist", "Sports Medicine Specialist", "Urologist", "Other"};
         swipeSelector.setItems(
                 new SwipeItem(0, categories[0], ""),
                 new SwipeItem(1, categories[1], ""),
@@ -46,10 +75,71 @@ public class DoctorSignup extends AppCompatActivity {
                 new SwipeItem(20, categories[20], "")
         );
 
-      /*  Intent doctor_signup = getIntent();
-        String Email =doctor_signup.getStringExtra(SignUpActivity.EXTRA_EMAIL);
-        String Password =doctor_signup.getStringExtra(SignUpActivity.EXTRA_PASS);
 
-        Toast.makeText(this, Email +"" +Password, Toast.LENGTH_SHORT).show();*/
+        AddressTextView = (MultiAutoCompleteTextView) findViewById(R.id.AddressTextView);
+        WorkNumTextView = (AutoCompleteTextView) findViewById(R.id.WorkNumTextView);
+        TagsTextView = (AutoCompleteTextView) findViewById(R.id.TagsTextView);
+        AddTagButton = (Button) findViewById(R.id.AddTagButton);
+        BioTextView = (MultiAutoCompleteTextView) findViewById(R.id.BioTextView);
+        FinishButton = (Button) findViewById(R.id.FinishButton);
+
+        FinishButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                RegisterDoctor(Email, Password,Name,Num);
+
+            }
+        });
+
     }
+
+    private void RegisterDoctor(String Email, String Password, final String FullName, final String Number) {
+
+        //TODO add validation for category
+        //String category = .getText().toString().trim();
+        String Address = AddressTextView.getText().toString().trim();
+        String WorkNum = WorkNumTextView.getText().toString().trim();
+        //TODO count tags for validation (minimum is 3)
+        //String Tags = TagsTextView.getText().toString().trim();
+        String Bio = BioTextView.getText().toString().trim();
+
+        if (TextUtils.isEmpty(Address)) {
+            Toast.makeText(this, "Please enter your adress", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(WorkNum)) {
+            Toast.makeText(this, "Please enter work phone number", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(Bio)) {
+            Toast.makeText(this, "Please enter your bio", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        firebaseAuth.createUserWithEmailAndPassword(Email, Password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+
+                            Toast.makeText(DoctorSignup.this, "Sign up completed", Toast.LENGTH_SHORT).show();
+                            mDatabase = FirebaseDatabase.getInstance().getReference();
+                            mDatabase.child("doctors").child(firebaseAuth.getCurrentUser().getUid()).child("full name").setValue(FullName);
+                            mDatabase.child("doctors").child(firebaseAuth.getCurrentUser().getUid()).child("phone number").setValue(Number);
+                            mDatabase.child("doctors").child(firebaseAuth.getCurrentUser().getUid()).child("address").setValue(AddressTextView.getText().toString().trim());
+                            mDatabase.child("doctors").child(firebaseAuth.getCurrentUser().getUid()).child("work number").setValue(WorkNumTextView.getText().toString().trim());
+                            mDatabase.child("doctors").child(firebaseAuth.getCurrentUser().getUid()).child("bio").setValue(BioTextView.getText().toString().trim());
+
+                            //Intent login = new Intent(DoctorSignup.this, //TODO here we put the doctors main activity);
+                           // startActivity(login);
+                        } else {
+
+                            //Toast.makeText(SignUpActivity.this, "Sign up failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+
 }
