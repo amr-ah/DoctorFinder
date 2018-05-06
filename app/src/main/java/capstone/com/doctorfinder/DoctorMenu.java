2af5add011273d3bec4518552b3eb84d566e4b07
anass-lahlal <anass.lahlal@gmail.com>
2018-05-06 20:45:42 +0300

    View for when doctor login to the app

---
+++ app/src/main/java/capstone/com/doctorfinder/DoctorMenu.java
@@ -0,0 +1,13 @@
+package capstone.com.doctorfinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DoctorMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_menu);
+    }
+}
