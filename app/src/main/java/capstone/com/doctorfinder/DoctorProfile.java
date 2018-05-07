c0c3d82e66b39220185fbb7cf0de1173f31481dd
anass-lahlal <anass.lahlal@gmail.com>
2018-05-08 01:11:06 +0300

    Doctor Profile (not finished)

---
+++ app/src/main/java/capstone/com/doctorfinder/DoctorProfile.java
@@ -0,0 +1,17 @@
+package capstone.com.doctorfinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DoctorProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_profile);
+
+
+        //TODO check the documentation for the expander in here https://android-arsenal.com/details/1/6662
+
+    }
+}
