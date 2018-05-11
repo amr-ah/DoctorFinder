b8a93de8fe5245878fba525fd409c1e735222dbc
anass-lahlal <anass.lahlal@gmail.com>
2018-05-11 14:30:41 +0300

    correct doctor profile text

--- app/src/main/java/capstone/com/doctorfinder/DoctorProfile.java
+++ app/src/main/java/capstone/com/doctorfinder/DoctorProfile.java
@@ -3,8 +3,12 @@ package capstone.com.doctorfinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.hsalf.smilerating.BaseRating;
+import com.hsalf.smilerating.SmileRating;
+
 public class DoctorProfile extends AppCompatActivity {
 
+    SmileRating mSmileRating;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -12,5 +16,7 @@ public class DoctorProfile extends AppCompatActivity {
 
         //TODO check the documentation for the expander in here https://android-arsenal.com/details/1/6662
 
+        mSmileRating = findViewById(R.id.smileyRating);
+        mSmileRating.setSelectedSmile(BaseRating.OKAY);
     }
 }
