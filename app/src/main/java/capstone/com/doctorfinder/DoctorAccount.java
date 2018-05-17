71ced650aab363501b5c3336d3da8f7e6bbe305f
amr sharqawi <amro.ah@hotmail.com>
2018-05-18 02:15:00 +0300

    data populated in doctors account

--- app/src/main/java/capstone/com/doctorfinder/DoctorAccount.java
+++ app/src/main/java/capstone/com/doctorfinder/DoctorAccount.java
@@ -6,6 +6,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.MultiAutoCompleteTextView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import org.ankit.gpslibrary.MyTracker;
 
@@ -19,10 +29,51 @@ public class DoctorAccount extends AppCompatActivity {
     int LOCATION_ACCESS;
     double longitude, latitude;
 
+    TextView name;
+    AutoCompleteTextView oldAddress;
+    AutoCompleteTextView oldPersonalPhone;
+    AutoCompleteTextView oldWorkPhone;
+    MultiAutoCompleteTextView oldbio;
+
+    private DatabaseReference mDatabase;
+    private FirebaseAuth firebaseAuth;
+    String D_ID;
+    String addressString;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_account);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        firebaseAuth = firebaseAuth.getInstance();
+        name = findViewById(R.id.oldName);
+        oldPersonalPhone = findViewById(R.id.oldPersonalPhone);
+        oldWorkPhone = findViewById(R.id.oldWorkPhone);
+        oldAddress = findViewById(R.id.oldAddress);
+        oldbio = findViewById(R.id.oldBio);
+
+        D_ID= firebaseAuth.getCurrentUser().getUid().toString().trim();
+        mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+
+                name.setText(snapshot.child("doctors").child(D_ID).child("full name").getValue(String.class));
+                oldbio.setText(snapshot.child("doctors").child(D_ID).child("bio").getValue(String.class));
+                oldPersonalPhone.setText(snapshot.child("doctors").child(D_ID).child("phone number").getValue(String.class));
+                oldWorkPhone.setText(snapshot.child("doctors").child(D_ID).child("work number").getValue(String.class));
+                addressString = snapshot.child("doctors").child(D_ID).child("address").getValue(String.class);
+                oldAddress.setText(addressString);
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
 
 
     }
@@ -31,17 +82,14 @@ public class DoctorAccount extends AppCompatActivity {
 
         String[] perms = {android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION};
 
-        if (EasyPermissions.hasPermissions(this,perms))
-        {
+        if (EasyPermissions.hasPermissions(this, perms)) {
             MyTracker position = new MyTracker(this);
 
             longitude = position.getLongitude();
             latitude = position.getLatitude();
-           Log.e("Permission",longitude + " " + latitude);
-        }
-        else
-        {
-            EasyPermissions.requestPermissions(this,"In order to access your location, you need to approve the permission !",LOCATION_ACCESS,perms);
+            Log.e("Permission", longitude + " " + latitude);
+        } else {
+            EasyPermissions.requestPermissions(this, "In order to access your location, you need to approve the permission !", LOCATION_ACCESS, perms);
         }
 
         //TODO Save longitude and latitude in the database.
